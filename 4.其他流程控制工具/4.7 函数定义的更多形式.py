7d47bfa1b8e34e208b3559f5223d8148aafb67be
watalo <watalo@163.com>
2020-10-04 01:18:21 +0800

    ??

--- 4.其他流程控制工具/4.7 函数定义的更多形式.py
+++ 4.其他流程控制工具/4.7 函数定义的更多形式.py
@@ -57,12 +57,12 @@ def parrot(voltage, state='a stiff', action = 'voom', type = 'Norwegian Blue'):
     print("-- Lovely plumage,the", type)
     print("--It's", state, '!')
 
-parrot(1000)                                          # 1 positional argument
-parrot(voltage=1000)                                  # 1 keyword argument
-parrot(voltage=1000000, action='VOOOOOM')             # 2 keyword arguments
-parrot(action='VOOOOOM', voltage=1000000)             # 2 keyword arguments
-parrot('a million', 'bereft of life', 'jump')         # 3 positional arguments
-parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword
+parrot(1000)                                          # 1 位置参数
+parrot(voltage=1000)                                  # 1 使用一个关键字参数
+parrot(voltage=1000000, action='VOOOOOM')             # 2 个关键字参数
+parrot(action='VOOOOOM', voltage=1000000)             # 2 两个关键字参数可以不考虑位置
+parrot('a million', 'bereft of life', 'jump')         # 3 不使用关键字的时候要注意位置的顺序
+parrot('a thousand', state='pushing up the daisies')  # 1 只要是有位置参数，就要考虑位置参数的顺序
 
 
 # 但下面的函数调用都是无效的:
