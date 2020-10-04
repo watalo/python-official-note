8c615ea63d387fdd9491c193cb5fdc85a1905751
watalo <watalo@163.com>
2020-10-04 14:55:19 +0800

    chap 4 finished

--- 4.其他流程控制工具/4.7-1 函数定义的更多形式.py
+++ 4.其他流程控制工具/4.7-1 函数定义的更多形式.py
@@ -112,3 +112,12 @@ cheeseshop('Limburger',
             shopkeeper='Michael Palin', 
             client = 'John cleese', 
             sketch="cheese shop sketch")   
+
+def fod(var, *args ,**kwargs):
+    print('var变量的值是{}'.format(var))
+    print('*args变量接收一个元组，接受多个参数，比如{}'.format([*args]))
+    print("**kwargs变量接受一个字典，这次接收的参数如下：")
+    for a in kwargs:
+        print(a,':',kwargs[a])
+
+fod(1, 2,3, a= 9)
